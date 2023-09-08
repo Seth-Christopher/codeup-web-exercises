797fb0eaa7d5ae2ec74483b6e417248741a94be1
Seth Martinez <sethchristopher.mtz@gmail.com>
2023-09-08 10:25:41 -0500

    alert and confirm exercise

---
+++ javascript-with-html.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let userChoice = confirm("Wait don't leave me yet! I chat with you yes?");
+
+if (userChoice === true){
+    console.log("yea baby");
+} else {
+    console.log("booo");
+}
+
