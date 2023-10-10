6d27e211e71b092ec753c536464b7179ce170f5b
Seth Martinez <sethchristopher.mtz@gmail.com>
2023-10-10 16:03:08 -0500

    completed profileCard Attribute and CSS exercises

---
+++ js/attributes-script.js
@@ -0,0 +1,36 @@
+// Two seconds after the page loads, change the "profile-pic" src attribute to a different image.
+function changeProfilePic () {
+    let img = document.querySelector('#profile-card img');
+    img.src = "https://willetspen.com/cdn/shop/collections/gobirds.jpg?v=1674608757";
+}
+// setInterval(changeProfilePic, 2000);
+
+
+//     Four seconds after the page loads, use innerHTML to change the name in "profile-name".
+function changeProfileName () {
+    let profileName = document.querySelector('#profile-name');
+    profileName.innerHTML = "Dirty Birds";
+}
+// setInterval(changeProfileName, 4000);
+
+//     Six seconds after page loads, add a new class to "profile-desc" that changes the color and font of the description text.
+function changeProfileDesc () {
+    let profileDesc = document.querySelector('#profile-desc');
+    profileDesc.style.color = "#036a38"; font = "monospace";
+}
+// setInterval(changeProfileDesc, 6000);
+
+//     Write code that toggles a class on the "profile-card" that changes its background color every two seconds. Use setInterval.
+function toggleBoggle () {
+    let profileCardToggle = document.querySelector('#profile-card');
+    profileCardToggle.classList.toggle('profile-card');
+}
+    // setInterval(toggleBoggle, 7000);
+
+function allTimer () {
+    setInterval(changeProfilePic, 1000);
+    setInterval(changeProfileName, 2000);
+    setInterval(changeProfileDesc, 3000);
+    setInterval(toggleBoggle, 4000);
+}
+allTimer();
