7df66ccec0445685f2d75d40de0743d83a582076
Seth Martinez <sethchristopher.mtz@gmail.com>
2023-10-11 10:53:38 -0500

    completed profileCard and attribute exercise

--- js/attributes-script.js
+++ js/attributes-script.js
@@ -28,9 +28,49 @@ function toggleBoggle () {
     // setInterval(toggleBoggle, 7000);
 
 function allTimer () {
-    setInterval(changeProfilePic, 1000);
+    setInterval(changeProfilePic, 1200);
     setInterval(changeProfileName, 2000);
     setInterval(changeProfileDesc, 3000);
-    setInterval(toggleBoggle, 4000);
+    setTimeout(toggleBoggle, 4000);
 }
 allTimer();
+
+
+// BELOW IS MY ATTEMPT AT A RANDOM BACKGROUND COLOR GENERATOR
+
+
+const hex = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','F']
+
+let btn = document.querySelector('#toggle-btn');
+
+//addEventListener
+btn.addEventListener("click", (event) => {
+
+    console.log(event)
+
+    let randomColor = "#"
+
+    for (let i = 0; i < 6; i++) {
+        randomColor += hex[getRandomNumber()]
+    }
+
+    document.body.style.backgroundColor  = randomColor;
+})
+function getRandomNumber() {
+
+    return Math.floor(Math.random() * hex.length)
+
+}
+
+
+
+// function setColor() {
+//     let x = document.body;
+//     x.style.backgroundColor = x.style.backgroundColor == "yellow" ? "pink" : "yellow";
+// }
+// setInterval(setColor, 2000);
+//
+// function stopColor() {
+//     clearInterval(myInterval);
+// }
+
