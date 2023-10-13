bd533a9f17988f2e4b9b2e2a6b80666a994c56c9
Seth Martinez <sethchristopher.mtz@gmail.com>
2023-10-13 16:09:20 -0500

    konami code exercise complete. javiers lecture update. parks traverse exercise complete

--- js/javiers.traverse.lecture.js
+++ js/javiers.traverse.lecture.js
@@ -1,20 +1,58 @@
-// const editButtons = document.querySelector(".gizmo button");
-const editButtons = document.querySelectorAll(".gizmo button");
+const editButtons = document.querySelectorAll("button.edit");
 
-function handleEditButtonClick(event){
-    const cardElements = event.target.parentElement.children;
-    for (let cardElement of cardElements) {
-        cardElement.addEventListener('mouseenter', ()=> {
-            cardElement.style.transform = "scale(1.2)";
-            cardElement.style.backgroundColor = "rgba(91, 83, 83, 1.0)";
-            cardElement.style.transition = "transform 0.4s ease-in, background-color 0.4s ease-in";
-        });
-        cardElement.addEventListener('mouseout', ()=> {
-            cardElement.style.transform = "unset";
-            cardElement.style.backgroundColor = "unset";
-        });
-    }
-}
+const addCardButton = document.querySelector("header button");
+
+const addCardSubmitButton = document.querySelector("div#addCardFormWrapper button");
+
+const removeButtons = document.querySelectorAll("button.remove");
+
+/* adding button click to toggle form on and off */
+addCardButton.addEventListener('click', () =>{
+    document.querySelector("#addCardFormWrapper").classList.toggle('hideForm');
+    document.querySelector("#addCardFormWrapper").classList.toggle('showForm');
+
+})
+
+addCardSubmitButton.addEventListener('click', event => {
+    event.preventDefault();
+    const newCardTitle = document.querySelector("#title").value;
+
+    const newCardImageLink = document.querySelector("#image").value;
+
+    const newCardDescription = document.querySelector("#description").value;
+
+    const newCard = document.createElement("div");
+    newCard.classList.add('gizmo');
+
+    // Step 1: create the element
+    const newCardH2Element = document.createElement("h2");
+    // Step 2: use JS methods to define the element's attributes and content
+    newCardH2Element.innerText = newCardTitle;
+    // Step 3: add the element to the DOM
+    newCard.appendChild(newCardH2Element);
+
+    const newCardImgElement = document.createElement("img");
+    newCardImgElement.src = newCardImageLink;
+    newCardImgElement.alt = newCardTitle;
+    newCard.appendChild(newCardImgElement);
+
+    const newCardParagraphElement = document.createElement("p");
+    newCardParagraphElement.innerText = newCardDescription;
+    newCard.appendChild(newCardParagraphElement);
+
+    const newCardEditButton = document.createElement("button");
+    newCardEditButton.innerText = "Edit";
+    newCardEditButton.addEventListener('click', handleEditButtonClick);
+    newCard.appendChild(newCardEditButton);
+
+    const newCardRemoveButton = document.createElement("button");
+    newCardRemoveButton.innerText = "Remove";
+    newCardRemoveButton.addEventListener('click', handleRemoveButtonClick);
+    newCard.appendChild(newCardRemoveButton);
+
+    document.querySelector("#gizmos").appendChild(newCard);
+    addCardButton.click();
+})
 
 function handleEditButtonClick(event){
     const headingText = event.target.parentElement.firstElementChild.innerText;
@@ -33,3 +71,31 @@ function handleEditButtonClick(event){
 editButtons.forEach(editButton => {
     editButton.addEventListener('click', handleEditButtonClick);
 });
+
+function handleRemoveButtonClick (event) {
+    if(confirm("Are you sure?")){
+        event.target.parentElement.remove();
+    }
+}
+
+removeButtons.forEach(removeButton => {
+    removeButton.addEventListener('click', handleRemoveButtonClick);
+});
+
+
+/*------ alternate handleEditButtonClick function ------- */
+
+// function handleEditButtonClick(event){
+//     const cardElements = event.target.parentElement.children;
+//     for (let cardElement of cardElements) {
+//         cardElement.addEventListener('mouseenter', ()=> {
+//             cardElement.style.transform = "scale(1.2)";
+//             cardElement.style.backgroundColor = "rgba(91, 83, 83, 1.0)";
+//             cardElement.style.transition = "transform 0.4s ease-in, background-color 0.4s ease-in";
+//         });
+//         cardElement.addEventListener('mouseout', ()=> {
+//             cardElement.style.transform = "unset";
+//             cardElement.style.backgroundColor = "unset";
+//         });
+//     }
+// }
---
+++ js/parks_traverse.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const highlightButtons = document.querySelectorAll(".national-parks button");
+
+const headersBold = document.querySelectorAll(".national-parks h3");
+
+function lastListItemHighlight (event) {
+    console.log(event.target.parentElement.firstElementChild.nextElementSibling.lastElementChild);
+
+    const lastLi = event.target.parentElement.firstElementChild.nextElementSibling.lastElementChild;
+
+    lastLi.style.backgroundColor = 'yellow';
+    // lastLi.addEventListener('click', function() {
+    //   this.style.backgroundColor = 'yellow';
+    // })
+}
+highlightButtons.forEach(highlightButton => {
+    highlightButton.addEventListener('click', lastListItemHighlight);
+});
+
+function headerBolder (event) {
+    console.log(event.target)
+    const lisBold = event.target.parentElement.firstElementChild.nextElementSibling;
+    lisBold.style.fontWeight = 'bold';
+}
+headersBold.forEach(headerClick => {
+    headerClick.addEventListener('click', headerBolder);
+});
+
+
+/*JAVIERS EXAMPLE FOR CREATING A BOLD STYLE TO BODY OF PARAGRAPH WHEN CLICKING THE <H3>
+
+document.querySelectorAll("h3").forEach(h3 => {
+    h3.addEventListener('click', () => {
+        h3.nextElementSibling.style.fontWeight = "bold";
+    });
+});*/
+
+
+// When any list item is clicked, the first li of that list item's parent ul should have a font color of blue
+document.querySelectorAll("li").forEach(li => {
+    li.addEventListener('click', () => {
+        li.parentElement.firstElementChild.style.color = "blue";
+    });
+});
