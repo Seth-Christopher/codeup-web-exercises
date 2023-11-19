9445dc1e6c79f9bd726176d153b1a661fcf1abf1
Seth Martinez <sethchristopher.mtz@gmail.com>
2023-11-18 20:20:39 -0700

    created a dom-assessment-practice html & js file.

---
+++ js/dom-assessment-practice.js
@@ -0,0 +1,22 @@
+// -1- When the user clicks the button with the id of change-heading, the content of the h1 element with the id of main-heading changes from DOM TO I control the DOM.
+
+
+// -2- When the user clicks on the button with the id of getInput, the content of the input with the id of userInput shows up in the paragraph with the id of showInput.
+
+
+// -3- When the user's mouse enters the section with the id of changes, all the text in that section becomes color hotpink.
+
+
+// -4- When the user's mouse enters the section with the id of serifChanger, the section gets the class sans-serif added to it.
+
+// -5- When the user clicks the button with the id of newParagraph, a paragraph identical to the first one (<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda enim hic illum impedit ipsa iure mollitia nam possimus quae qui? Atque dolores laudantium, minus officiis pariatur quo repellendus soluta ullam.</p>) is appended to the section with the id of moreParagraphs.
+
+// -6- When the user's mouse enters the section with the id of big-and-small, the section gets the class go-big toggled on if it's off and off if it's on.
+
+// -7- Three seconds after the page loads, a paragraph with the content "Document Object Model" appears under the main heading in the header element.
+
+// -8- When the user's mouse enters the Codeup logo, the image changes from img/logo.png to img/OfficialCodeupLogo.png. Hint: modify the value of the src attribute.
+
+// -9- When the user clicks on the last "Click Me" button, all the h2 elements in any section on the page start disappearing and reappearing every second.
+
+// -10- When the user clicks on a Click Me button, the text in the corresponding paragraph with the text "Change Me" changes to "You changed me!". For example, if the first button is clicked, the first paragraph changes. If the second paragraph is clicked, the second paragraph changes, and so forth.
--- js/fetch.js
+++ js/fetch.js
@@ -7,8 +7,60 @@ const ghOptions = {
     }
 }
 
-fetch("https://api.github.com/users", ghOptions)
-    .then(response => response.json())
-    .then(data => console.log(data));
+let users = [];
+let usersDiv = document.querySelector("#users")
+function getUsers () {
+    fetch("https://api.github.com/users", ghOptions)
+        .then(response => response.json())
+        .then(data => {
+            users = data;
+
+            loopingThroughUsers();
+        })
+}
+
+function loopingThroughUsers (){
+    users.forEach(user => {
+        console.log(user);
+        let card = displayUserData(user);
+        // usersDiv.appendChild(card);
+        })
+    }
+getUsers();
+function displayUserData(user) {
+    // console.log(user);
+    // create a new card for each user
+    const newCard = document.createElement("div");
+    newCard.classList.add("user");
+    newCard.dataset.id = user.id;
+
+    // create elements for the id, nodeID, and user login
+    const userLogin =document.createElement("h4");
+    // userLogin.textContent = user.login;
+
+    const userID = document.createElement("h5");
+    // userID.textContent = user.id;
+    const nodeID = document.createElement("h5");
+    // nodeID.textContent = user.node_id;
+
+
+    // append the elements to the card
+
+    newCard.appendChild(userLogin);
+    newCard.appendChild(userID);
+    newCard.appendChild(nodeID);
+
+    // append the card to the usersDiv
+    usersDiv.appendChild(newCard);
+}
+displayUserData();
+
+// const usersLogin = data.login;
+//
+// const usersDiv = document.querySelector("#users").innerHTML +=
+//     `<h1>${usersLogin}</h1>
+//     `;
+//
+
 
 
